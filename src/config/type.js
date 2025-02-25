484c947ddc3f17e892d9ce40c422b4438d5fc901
alzcan <109632909+alzcan@users.noreply.github.com>
2025-02-26 00:38:02 +0300

    type.js

--- src/config/type.js
+++ src/config/type.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💮 Distube Bot [ Free Tier ] ";
-const text = "GlaceYT";
+const botName = "Bang & Olufsen";
+const text = "";
 const version = "Latest@ v1.0";
 const startTime = Date.now();
 
